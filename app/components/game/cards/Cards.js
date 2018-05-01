48395b33ec5fa06018717e818836ac327935b1f0
andreobriennz <andre@communica.co.nz>
2018-05-01 17:23:33 +1200

    Cards component set up to work as native

--- app/components/game/cards/Cards.js
+++ app/components/game/cards/Cards.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {StyleSheet, Text, View } from 'react-native';
+
 import { Card } from './Card'
 import {info} from './../../Info'
 
@@ -6,25 +8,25 @@ class Cards extends React.Component {
 
     render() {
         return (
-            <section className="cards">
-                <strong className="large">Your Cards</strong>
+            <View className="section cards">
+                <Text className="strong large">Your Cards</Text>
                 
-                <div className="cards">
+                <View className="cards">
                     {this.props.cards.map ( (card, index) => {
                         let description = `${description}          `
                         description = card.description.match(/\b[\w']+(?:[^\w\n]+[\w']+){0,1}\b/g)
 
                         return (
-                            <div
+                            <View
                             key={index} 
                             onClick={this.props.handlePlayCard.bind(this, card)}
                             className="card">
                                 <Card title={card.name} description={description} footer={card.effects} />
-                            </div>
+                            </View>
                         );
                     })}
-                </div>
-            </section>
+                </View>
+            </View>
         );
     }
 };
